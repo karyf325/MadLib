4f8f1aa1f29ce45dea912c7dd043da7943951cd8
karyf325 <43179791+karyf325@users.noreply.github.com>
2018-10-09 19:29:02 -0700

    Add files via upload

--- src/MadLib.java
+++ src/MadLib.java
@@ -7,31 +7,37 @@ public class MadLib
 	{
 	
 		Scanner keyboard = new Scanner(System.in);
-		System.out.print("Please enter a verb ending in 'ING':  ");
+		System.out.print ("Please enter a verb ending in 'ING':  ");
 		String verb = keyboard.nextLine();
-		System.out.print("Please enter a proper he or she:   "); 
+		System.out.print("\nPlease enter a proper he or she:   "); 
+		
 		String heshe = keyboard.nextLine();
-		System.out.print("Please enter a verb ending in 'ED':   ");
+		System.out.print("\nPlease enter a verb ending in 'ED':   ");
 		String verb1 = keyboard.nextLine();
-		System.out.print("Please enter an emotion:  ");
+		System.out.print("\nPlease enter an emotion:  ");
+		
 		String emotion = keyboard.nextLine();
-		System.out.print("Please enter a proper noun:    ");
+		System.out.print("\nPlease enter a proper noun:    ");
 		String propernoun1 = keyboard.nextLine();
-		System.out.print("Please enter a place:   ");
+		System.out.print("\nPlease enter a place:   ");
+		
 		String place = keyboard.nextLine();
-		System.out.print("Please enter a food:     ");
+		System.out.print("\nPlease enter a food:     ");
 		String food = keyboard.nextLine();
-		System.out.print("Please enter a play/musical:   ");
+		System.out.print("\nPlease enter a play/musical:   ");
+		
 		String playmusical = keyboard.nextLine();
-		System.out.print("Please enter a verb:    ");
+		System.out.print("\nPlease enter a verb:    ");
 		String verb2 = keyboard.nextLine();
-		System.out.print("Please enter a clothing item:    ");
+		System.out.print("\nPlease enter a clothing item:    ");
+		
 		String clothingitem = keyboard.nextLine();
 	
 		
-		System.out.print("I was" + verb + "in the car when " + heshe + verb1 + " at me " + "I was so" + emotion + "to see that it was" + propernoun1
-		+ "This year we are going to" + place + "When we get there we will eat a dozen"
-		+ food + "I want to go see" + playmusical + "and" + verb2 + "all day I will wear"
+		System.out.print("I was " + verb + " in the car when " + heshe + " "+
+		verb1 + " at me " + "I was so " + emotion + " to see that it was " + propernoun1
+		+ " This year we are going to " + place + " When we get there we will eat a dozen "
+		+ food + " I want to go see " + playmusical + " and " + verb2 + " all day I will wear "
 		+ clothingitem );
 		}
 }
